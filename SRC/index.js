8946004447de11a192a8e7eb1cf6e375dee5e672
Yami0602 <ysagales@techlaunch.io>
2017-11-07 18:30:32 -0500

    first commit

---
+++ SRC/index.js
@@ -0,0 +1,15 @@
+
+    // Lodash, now imported by this script
+
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+  index.html
